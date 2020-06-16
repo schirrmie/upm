6a6be206d1f9e2cea67aeb92ab54f9b0b565a098
Alexander Doering <adoering@siv.de>
2020-06-16 11:17:16 +0200

    Deleted double class

--- UpdateCommands.php
+++ UpdateCommands.php
@@ -21,21 +21,6 @@
   require_once 'ServerCommands.php';
   require_once 'ConfigCommands.php';
   
-  abstract class CommandNames {
-    const Distribution = "distribution_command";
-    const DistributionVersion = "distribution_version_command";
-    const Uptime = "uptime_command";
-    const RestartRequired = "restart_command";
-    const ListUpdates = "updates_list_command";
-    const PatchInfo = "update_info_command";
-    const PatchChangelog = "update_changelog_command";
-    const UpdateSystem = "update_system_command";
-    const UpdatePackage = "update_package_command";
-    const RebootSet = "reboot_set_command";
-    const RebootGet = "reboot_get_command";
-    const RebootDel = "reboot_del_command";
-  }
-
   class UpdateCommands {
 
     private static $ssh;
